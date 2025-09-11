cbaa30402bbdbc04bff7cae6e813b18d725cf409
Arnav Khandelwal <arnav181104@gmail.com>
2025-09-11 21:33:36 +0530

    about section

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -14,14 +14,32 @@ const About = () => {
             />
           </div>
           <div className={styles.textWrapper}>
-            <h2 className={styles.title}>The Perch: Rest Easy, Live Naturally</h2>
+            <h2 className={styles.title}>Rest Easy, Live Naturally</h2>
              <p className={styles.text}>
                 Amidst the lush greenery of Kerala’s Kollam district, is this peaceful homestay that offers complete solitude and privacy, perfect for those seeking a quiet escape to reconnect with nature. Situated far from the crowds, the homestay features a cottage with a kitchenette, allowing you to unwind completely and embrace the tranquility of the surroundings
             </p>
             <p className={styles.text}>
-The air is filled with the soothing sounds of chirping birds, the rustling of fruit trees, and the occasional flutter of butterflies, making it a paradise for nature lovers. Unwind in total privacy -sip your morning tea with only birds for company, lose yourself in a book, or simply soak in the beauty of the serene surroundings. No noise, no distractions -just you, nature, and complete tranquility.
-For those who want to experience the rustic charm of the place, evening walks and light conversations with the simple, friendly locals will enrich the experience.
+                The air is filled with the soothing sounds of chirping birds, the rustling of fruit trees, and the occasional flutter of butterflies, making it a paradise for nature lovers. Unwind in total privacy -sip your morning tea with only birds for company, lose yourself in a book, or simply soak in the beauty of the serene surroundings. No noise, no distractions -just you, nature, and complete tranquility.
+                For those who want to experience the rustic charm of the place, evening walks and light conversations with the simple, friendly locals will enrich the experience.
             </p>
+          </div>
+        </div>
+        <div className={styles.flexRow}>
+          <div className={styles.textWrapperLeft}>
+            <h2 className={styles.title}>Serenity Waits for You</h2>
+             <p className={styles.text}>
+                Whether you’re looking for creative inspiration, peaceful rest, or just a break from the whirlwind of daily chores, this retreat provides a serene sanctuary to restore your energy and calm your mind, enjoy the quiet and a deep connection with nature.
+            </p>
+            <p className={styles.text}>
+                Whether you prefer lounging with a book, working from home , indulging in soulful conversations, or simply enjoying the stillness, this space is yours to recharge.
+            </p>
+          </div>
+          <div className={styles.imageWrapper}>
+            <img
+              src="/sides/2.jpeg"
+              alt="Kerala side view at The Perch"
+              className={styles.imageLeft}
+            />
           </div>
         </div>
       </div>
